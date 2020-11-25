2234142743e728ef73a1be64c4a564065391f608
oldUath <haiyangdong666@gmail.com>
2020-11-25 20:38:22 +0800

    登录注册优化动画

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,19 +6,23 @@
           <div class="main"></div>
           <div class="form">
             <h3 @click="showRegister">创建账户</h3>
-            <div v-show="isShowRegister" class="register">
-              <input type="text" v-model="register.username" placeholder="用户名" />
-              <input type="password" v-model="register.password" placeholder="密码" />
-              <p v-bind:class="{ error: register.isError }">{{ register.notice }}</p>
-              <div class="button" @click="onRegister">创建账号</div>
-            </div>
+            <transition name="slide">
+              <div v-bind:class="{show: isShowRegister}" class="register">
+                <input type="text" v-model="register.username" placeholder="用户名" />
+                <input type="password" v-model="register.password" placeholder="密码" />
+                <p v-bind:class="{ error: register.isError }">{{ register.notice }}</p>
+                <div class="button" @click="onRegister">创建账号</div>
+              </div>
+            </transition>
             <h3 @click="showLogin">登录</h3>
-            <div v-show="isShowLogin" class="login">
-              <input type="text" v-model="login.username" placeholder="输入用户名" />
-              <input type="password" v-model="login.password" placeholder="密码" />
-              <p v-bind:class="{ error: login.isError }">{{ login.notice }}</p>
-              <div class="button" @click="onLogin">登录</div>
-            </div>
+            <transition name="slide">
+              <div v-bind:class="{show:isShowLogin}" class="login">
+                <input type="text" v-model="login.username" placeholder="输入用户名" />
+                <input type="password" v-model="login.password" placeholder="密码" />
+                <p v-bind:class="{ error: login.isError }">{{ login.notice }}</p>
+                <div class="button" @click="onLogin">登录</div>
+              </div>
+            </transition>
           </div>
         </div>
       </div>
@@ -59,7 +63,7 @@ export default {
         this.register.isError = true;
         this.register.notice = "用户名3~15个字符，仅限于字母数字下划线中文";
         return;
-      }else if (!/^.{6,16}$/.test(this.register.password)) {
+      } else if (!/^.{6,16}$/.test(this.register.password)) {
         this.register.isError = true;
         this.register.notice = "密码长度为6~16个字符";
         return;
@@ -124,9 +128,11 @@ export default {
   .form {
     width: 270px;
     border-left: 1px solid #ccc;
+    overflow: hidden;
 
     h3 {
       padding: 10px 20px;
+      margin-top: -1px;
       font-weight: normal;
       font-size: 16px;
       border-top: 1px solid #eee;
@@ -151,8 +157,14 @@ export default {
 
     .login,
     .register {
-      padding: 10px 20px;
+      padding: 0 20px;
       border-top: 1px solid #eee;
+      height: 0;
+      overflow: hidden;
+      transition: height 0.4s;
+       &.show {
+        height: 193px;            
+      }
 
       input {
         display: block;
