75b840fd6ed1cc18774f1deb6019b136cf25cc63
oldUath <haiyangdong666@gmail.com>
2020-11-29 15:45:44 +0800

    笔记本列表页面的静态页面

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -1,27 +1,55 @@
 <template>
-    <div id="notebook-list">
-        <h1>{{msg}}</h1>
-        <ul>
-            <li><router-link to="/note/1">笔记本1</router-link></li>
-            <li><router-link to="/note/2">笔记本2</router-link></li>
-        </ul>
-    </div>
+  <div id="notebook-list" class="detail">
+    <header>
+      <a href="#" class="btn"><i class="iconfont icon-plus"></i>新建笔记本</a>
+    </header>
+    <main>
+        <div class="layout">
+            <h3>笔记本列表</h3>
+            <div class="book-list">
+                <a href="#" class="notebook"> 
+                    <div>
+                        <span class="iconfont icon-notebook">笔记本标题1<span class="count">3</span><span class="action">编辑</span></span>
+                        
+                        <span class="action">删除</span>
+                        <span class="date">3天谴</span>
+                    </div>
+                </a>
+                 <a href="#" class="notebook">
+                    <div>
+                        <span class="iconfont icon-notebook">笔记本标题2<span class="count">3</span><span class="action">编辑</span></span>
+                        
+                        <span class="action">删除</span>
+                        <span class="date">3天谴</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </main>
+  </div>
 </template>
 
-<script >
-import Auth from '@/apis/auth'
+<script>
+import Auth from "@/apis/auth";
 export default {
-    data(){
-        return{
-            msg:"笔记本列表"
-        }
-    },
-    created(){
-        Auth.getInfo().then(res=>{
-            if(!res.isLogin){
-                this.$router.push({path:'/login'})
-            }
-        })
-    }
-}
+  data() {
+    return {
+      msg: "笔记本列表",
+    };
+  },
+  created() {
+    Auth.getInfo().then((res) => {
+      if (!res.isLogin) {
+        this.$router.push({ path: "/login" });
+      }
+    });
+  },
+};
 </script>
+
+<style lang="less" scoped>
+    @import '../assets/css/notebookList.less';
+</style>
+
+
+
