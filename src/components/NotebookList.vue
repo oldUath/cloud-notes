c361393cb9fee27c73e578b220139b5e73aa3a7b
oldUath <haiyangdong666@gmail.com>
2020-11-29 23:13:08 +0800

    开始准备与用户交互

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -1,39 +1,39 @@
 <template>
   <div id="notebook-list" class="detail">
     <header>
-      <a href="#" class="btn"><i class="iconfont icon-plus"></i>新建笔记本</a>
+      <a href="#" class="btn" @click="onCreate"><i class="iconfont icon-plus"></i>新建笔记本</a>
     </header>
     <main>
-        <div class="layout">
-            <h3>笔记本列表</h3>
-            <div class="book-list">
-                <a href="#" class="notebook"> 
-                    <div>
-                        <span class="iconfont icon-notebook"></span>笔记本标题1<span >3</span><span class="action">编辑</span>
-                        
-                        <span class="action">删除</span>
-                        <span class="date">3天谴</span>
-                    </div>
-                </a>
-                <a href="#" class="notebook"> 
-                    <div>
-                        <span class="iconfont icon-notebook"></span>笔记本标题1<span >3</span><span class="action">编辑</span>
-                        
-                        <span class="action">删除</span>
-                        <span class="date">3天谴</span>
-                    </div>
-                </a>
+      <div class="layout">
+        <h3>笔记本列表</h3>
+        <div class="book-list">
+          <a href="#" class="notebook">
+            <div>
+              <span class="iconfont icon-notebook"></span>笔记本标题1<span>3</span><span class="action" @click="onEdit">编辑</span>
+
+              <span class="action" @click="onDelete">删除</span>
+              <span class="date">3天谴</span>
             </div>
+          </a>
+          <a href="#" class="notebook">
+            <div>
+              <span class="iconfont icon-notebook"></span>笔记本标题1<span>3</span><span class="action">编辑</span>
+
+              <span class="action">删除</span>
+              <span class="date">3天谴</span>
+            </div>
+          </a>
         </div>
+      </div>
     </main>
   </div>
 </template>
 
 <script>
 import Auth from "@/apis/auth";
-import Notebooks from '@/apis/notebook'
+import Notebooks from "@/apis/notebook";
 
-window.notebooks = Notebooks
+window.notebooks = Notebooks;
 export default {
   data() {
     return {
@@ -47,12 +47,20 @@ export default {
       }
     });
   },
+  methods: {
+    onCreate() {
+      console.log("create...");
+    },
+    onEdit(){
+        console.log('edit...')
+    },
+    onDelete(){
+        console.log('delete')
+    }
+  },
 };
 </script>
 
 <style lang="less" scoped>
-    @import '../assets/css/notebookList.less';
+@import "../assets/css/notebookList.less";
 </style>
-
-
-
