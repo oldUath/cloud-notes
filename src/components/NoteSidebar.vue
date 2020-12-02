18066e50ba1acb36afb42019fa9be8cd68bd3030
oldUath <haiyangdong666@gmail.com>
2020-12-02 18:18:09 +0800

    侧边栏显示笔记和笔记本完成

--- src/components/NoteSidebar.vue
+++ src/components/NoteSidebar.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="note-sidebar">
-    <span class="btn add-note"  >添加笔记</span>
-    <el-dropdown class="notebook-title"  @command="handleCommand" placement="bottom">
-      <span class="el-dropdown-link">
-        笔记本标题 <i class="iconfont icon-down"></i>
-      </span>
+    <span class="btn add-note">添加笔记</span>
+    <el-dropdown class="notebook-title" @command="handleCommand" placement="bottom">
+      <span class="el-dropdown-link"> {{ curBook.title }} <i class="iconfont icon-down"></i> </span>
       <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item v-for="(notebook,index) in notebooks" :key="index" 
-            :command="notebook.id">{{notebook.title}}
-        </el-dropdown-item>
-        <el-dropdown-item  command="trash">回收站</el-dropdown-item>
+        <el-dropdown-item v-for="(notebook, index) in notebooks" :key="index" :command="notebook.id">{{ notebook.title }} </el-dropdown-item>
+        <el-dropdown-item command="trash">回收站</el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
     <div class="menu">
@@ -18,9 +14,9 @@
     </div>
     <ul class="notes">
       <li v-for="note in notes" :key="note.id">
-        <router-link :to="`/note?noteId=${note.id}`">
-          <span class="date">{{note.updatedAtFriendly}}</span>
-          <span class="title">{{note.title}}</span>          
+        <router-link :to="`/note?noteId=${note.id}&notebookId=${curBook.id}`">
+          <span class="date">{{ note.updatedAtFriendly }}</span>
+          <span class="title">{{ note.title }}</span>
         </router-link>
       </li>
     </ul>
@@ -28,41 +24,43 @@
 </template>
 
 <script>
-  import Notebooks from '@/apis/notebook'
-  import Bus from '@/helpers/bus'
-  import Notes from '@/apis/note'
-  window.Notes = Notes
+import Notebooks from "@/apis/notebook";
+import Bus from "@/helpers/bus";
+import Notes from "@/apis/note";
+window.Notes = Notes;
 
-  export default {
-    created(){
-      Notebooks.getAll().then(res=>{
-        this.notebooks = res.data
-      })
-    },
-    data() {
-      return {
-        notebooks: [],
-        notes:[],
+export default {
+  created() {
+    Notebooks.getAll().then((res) => {
+      this.notebooks = res.data;
+      this.curBook = this.notebooks.find((notebook) => notebook.id == this.$route.query.notebookId) || this.notebooks[0] || {};
+      return Notes.getAll({ notebookId: this.curBook.id }).then((res) => {
+        this.notes = res.data;
+      });
+    });
+  },
+  data() {
+    return {
+      notebooks: [],
+      notes: [],
+      curBook: {}, //当前笔记本
+    };
+  },
+
+  methods: {
+    handleCommand(notebookId) {
+      if (notebookId == "trash") {
+        return this.$router.push({path:'/trash'})
       }
+      this.curBook = this.notebooks.find(notebook => notebook.id == notebookId)
+      Notes.getAll({ notebookId }).then((res) => {
+        this.notes = res.data;
+      });
     },
-
-    methods: {
-      handleCommand(notebookId){
-        if(notebookId !== 'trash'){
-          Notes.getAll({notebookId}).then(res=>{
-            this.notes = res.data
-          })
-        }
-      }
-    }
-  }
-
+  },
+};
 </script>
 
-
-<style lang="less" >
+<style lang="less">
 @import url(../assets/css/noteSidebar.less);
-
 </style>
-
-
