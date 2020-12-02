decf42158af2a58dbb076fd0435409d41812135a
oldUath <haiyangdong666@gmail.com>
2020-12-02 19:15:33 +0800

    详情页样式

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -1,16 +1,28 @@
 <template>
-  <div id="note">
-  <note-sidebar></note-sidebar>
-    <div id="note-detail">
-      <h1>notebookId: {{ $route.query.notebookId }}</h1>
-      <h1>noteId: {{ $route.query.noteId }}</h1>
+  <div id="note" class="detail">
+    <note-sidebar></note-sidebar>
+    <div class="note-detail">
+      <div class="note-bar">
+        <span>创建日期：2天前</span>
+        <span>更新日期：1s</span>
+        <span>已保存</span>
+        <span class="iconfont icon-delete"></span>
+        <span class="iconfont icon-fullscreen"></span>
+      </div>
+      <div class="note-title">
+        <input type="text" placeholder="请输入笔记名称" />
+      </div>
+      <div class="editor">
+        <textarea v-show="true" placeholder="输入内容支持markdown语法"></textarea>
+        <div class="preview markdown-body" v-html="false" v-show="false"></div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Auth from "@/apis/auth";
-import NoteSidebar from './NoteSidebar.vue';
+import NoteSidebar from "./NoteSidebar.vue";
 export default {
   components: { NoteSidebar },
   data() {
@@ -28,8 +40,10 @@ export default {
 };
 </script>
 
-<style scoped>
-#note{
+<style lang="less" >
+@import url(../assets/css/noteDetail.less);
+
+#note {
   display: flex;
   align-items: stretch;
   background: #fff;
